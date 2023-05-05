fe9c9424ac477ffc2da03f57960d9330fd055123
GwenaelleSa <gwenaelle.salamin@gmail.com>
2023-05-05 14:05:38 +0200

    Offer the possibility to produce transparent mask

--- scripts/image_processing/mask_for_buildings.py
+++ scripts/image_processing/mask_for_buildings.py
@@ -8,8 +8,9 @@ from yaml import load, FullLoader
 import geopandas as gpd
 import numpy as np
 import rasterio
+from rasterio.mask import mask
 from rasterio.features import rasterize
-from shapely.geometry import Polygon
+from shapely.geometry import Polygon, mapping
 
 sys.path.insert(1, 'scripts')
 import functions.fct_misc as fct_misc
@@ -42,6 +43,8 @@ with open('scripts/image_processing/config.yaml') as fp:
 
 logger.info('Defining constants...')
 
+TRANSPARENCY=cfg['transparency']
+
 WORKING_DIR=cfg['working_dir']
 ROOFS=cfg['roofs']
 LAYER=cfg['layer']
@@ -61,23 +64,37 @@ merged_roofs_geoms=roofs.unary_union
 
 for tile in tqdm(tiles, desc='Producing the masks', total=len(tiles)):
 
-    with rasterio.open(tile, "r") as src:
-        tile_img = src.read()
-        tile_meta = src.meta
+    if TRANSPARENCY:
+        geoms_list = [mapping(merged_roofs_geoms)]
 
-    im_size = (tile_meta['height'], tile_meta['width'])
+        with rasterio.open(tile) as src:
+            mask_image, mask_transform = mask(src, geoms_list)
+            mask_meta=src.meta
 
-    polygons=[poly_from_utm(geom, src.meta['transform']) for geom in merged_roofs_geoms.geoms]
-    mask = rasterize(shapes=polygons, out_shape=im_size)
+        mask_meta.update({'transform': mask_transform})
+        filepath=os.path.join(fct_misc.ensure_dir_exists(os.path.join(IMAGE_FOLDER, 'masked_images')),
+                            tile.split('/')[-1].split('.')[0] + '_masked.tif')
+        
+        with rasterio.open(filepath, 'w', **mask_meta) as dst:
+            dst.write(mask_image)
 
-    mask_meta = src.meta.copy()
-    mask_meta.update({'count': 1, 'dtype': 'uint8', 'nodata':99})
+    else:
+        with rasterio.open(tile, "r") as src:
+            tile_img = src.read()
+            tile_meta = src.meta
 
-    filepath=os.path.join(fct_misc.ensure_dir_exists(os.path.join(IMAGE_FOLDER, 'mask')),
-                            tile.split('/')[-1].split('.')[0] + '_mask.tif')
-    
+        im_size = (tile_meta['height'], tile_meta['width'])
 
-    with rasterio.open(filepath, 'w', **mask_meta) as dst:
-        dst.write(mask,1)
+        polygons=[poly_from_utm(geom, src.meta['transform']) for geom in merged_roofs_geoms.geoms]
+        mask_image = rasterize(shapes=polygons, out_shape=im_size)
+
+        mask_meta = src.meta.copy()
+        mask_meta.update({'count': 1, 'dtype': 'uint8', 'nodata':99})
+
+        filepath=os.path.join(fct_misc.ensure_dir_exists(os.path.join(IMAGE_FOLDER, 'mask')),
+                                tile.split('/')[-1].split('.')[0] + '_mask.tif')    
+
+        with rasterio.open(filepath, 'w', **mask_meta) as dst:
+            dst.write(mask_image, 1)
 
 logger.success(f'The masks were written in the folder {os.path.join(IMAGE_FOLDER, "mask")}.')
