f070ce9f463a05c166f2bcee9d1d9d5af89b444c
GwenaelleSa <gwenaelle.salamin@gmail.com>
2023-06-06 17:28:41 +0200

    Filter by roughness

--- scripts/lidar_intensity/filter_surfaces_by_attributes.py
+++ scripts/lidar_intensity/filter_surfaces_by_attributes.py
@@ -48,7 +48,7 @@ def get_tilepath_from_id(tile_id, im_list):
     '''
 
     matching_path=[tilepath for tilepath in im_list if tile_id in tilepath]
-    if len(matching_path)>1:
+    if len(matching_path)>2:
         logger.critical(f'There are multiple tiles corresponding to the id {tile_id}.')
         sys.exit(1)
     else:
@@ -72,11 +72,15 @@ NODATA_OVERLAP=0.25
 LIM_STD=5500
 LIM_MOE=400
 Z=2
+LIM_ROUGHNESS=7.5
+
+STAT_LIMITS={'MOE_i': LIM_MOE, 'std_i': LIM_STD, 'median_r': LIM_ROUGHNESS}
 
 os.chdir(WORKING_DIR)
 
 logger.info('Getting the files...')
-im_list=glob(os.path.join(INPUT_DIR_IMAGES, '*.tif'))
+im_list_intensity=glob(os.path.join(INPUT_DIR_IMAGES, 'intensity', '*.tif'))
+im_list_roughness=glob(os.path.join(INPUT_DIR_IMAGES, 'roughness', '*.tif'))
 lidar_tiles=gpd.read_file(LIDAR_TILES)
 roofs=gpd.read_file(ROOFS)
 
@@ -108,9 +112,9 @@ logger.info('Getting zones with nodata value...')
 nodata_polygons=[]
 for tile_id in tqdm(lidar_tiles['id'].values, desc='Getting nodata area on tiles...'):
 
-    if any(tile_id in tilepath for tilepath in im_list):
+    if any(tile_id in tilepath for tilepath in im_list_intensity):
 
-        tilepath=get_tilepath_from_id(tile_id, im_list)
+        tilepath=get_tilepath_from_id(tile_id, im_list_intensity)
 
         with rasterio.open(tilepath, crs='EPSG:2056') as src:
             intensity=src.read(1)
@@ -129,6 +133,7 @@ nodata_overlap['joined_area']=nodata_overlap.geometry.area
 nodata_overlap_grouped=nodata_overlap[['OBJECTID', 'tile_id', 'joined_area']].groupby(by=['OBJECTID', 'tile_id']).sum().reset_index()
 nodata_overlap_full=gpd.GeoDataFrame(nodata_overlap_grouped.merge(existing_clipped_roofs, how='right', on=['OBJECTID', 'tile_id']), crs='EPSG:2056')
 nodata_overlap_full['nodata_overlap']=nodata_overlap_full['joined_area']/nodata_overlap_full['clipped_area']
+nodata_overlap_full=nodata_overlap_full.round(3)
 
 logger.info('Excluding roofs with too much area not classifed as building...')
 
@@ -144,7 +149,7 @@ other_classes_roofs=cause_occupation(other_classes_roofs, f'More than {NODATA_OV
 
 clipped_roofs_cleaned=building_roofs[(building_roofs['nodata_overlap']<=NODATA_OVERLAP) | (nodata_overlap_full['nodata_overlap'].isna())].reset_index(drop=True)
 
-logger.info(f'{no_roofs.shape[0]} roofs are classified as undefined, because they do not overlap enough with the building class.')
+logger.info(f'{no_roofs.shape[0]} roofs are classified as undefined, because they do not overlap with the building class at more than 75%.')
 logger.info(f'{other_classes_roofs.shape[0]} roofs are classified as occupied, '+
             f'because more than {NODATA_OVERLAP*100}% of their surface is not classified as building.')
 
@@ -154,17 +159,15 @@ del nodata_overlap, nodata_overlap_grouped, nodata_overlap_full
 del building_roofs
 
 # Make stats per polygon
-zs_per_clear_roofs=gpd.GeoDataFrame()
-roofs_high_variability=gpd.GeoDataFrame()
-roofs_high_std=gpd.GeoDataFrame()
-roofs_high_moe=gpd.GeoDataFrame()
+zs_per_roof=gpd.GeoDataFrame()
 for tile_id in tqdm(lidar_tiles['id'].values, desc='Getting zonal stats from tiles...'):
 
-    if any(tile_id in tilepath for tilepath in im_list):
+    if any(tile_id in tilepath for tilepath in im_list_intensity) and any(tile_id in tilepath for tilepath in im_list_intensity):
 
         roofs_on_tile=clipped_roofs_cleaned[clipped_roofs_cleaned['tile_id']==tile_id].reset_index(drop=True)
 
-        tilepath=get_tilepath_from_id(tile_id, im_list)
+        # Intensity statistics
+        tilepath=get_tilepath_from_id(tile_id, im_list_intensity)
         
         with rasterio.open(tilepath, crs='EPSG:2056') as src:
             intensity=src.read(1)
@@ -172,37 +175,65 @@ for tile_id in tqdm(lidar_tiles['id'].values, desc='Getting zonal stats from til
         
         meta.update({'crs': rasterio.crs.CRS.from_epsg(2056)})
 
-        zs_df=pd.DataFrame(zonal_stats(roofs_on_tile, intensity, affine=meta['transform'],
+        zs_df_intensty=pd.DataFrame(zonal_stats(roofs_on_tile, intensity, affine=meta['transform'],
                                         stats=['min', 'max', 'mean', 'median', 'std', 'count'], nodata=meta['nodata']))
-        zs_per_roof=pd.concat([roofs_on_tile, zs_df], axis=1)
+        zs_per_roof_on_tile=pd.concat([roofs_on_tile, zs_df_intensty], axis=1)
+        zs_per_roof_on_tile.rename(columns={'min': 'min_i', 'max': 'max_i', 'mean': 'mean_i', 'median': 'median_i',
+                                            'std': 'std_i', 'count': 'count_i'}, inplace=True)
+    
+        # Roughness statistics
+        tilepath=get_tilepath_from_id(tile_id, im_list_roughness)
+        
+        with rasterio.open(tilepath, crs='EPSG:2056') as src:
+            roughness=src.read(1)
+            meta=src.meta
+        
+        meta.update({'crs': rasterio.crs.CRS.from_epsg(2056)})
 
-        # Check the marge or error
-        zs_per_roof['MOE'] = Z*zs_per_roof['std']/(zs_per_roof['count']**(1/2))
+        zs_df_roughness=pd.DataFrame(zonal_stats(roofs_on_tile, roughness, affine=meta['transform'],
+                                        stats=['min', 'max', 'mean', 'median', 'std', 'count'], nodata=meta['nodata']))
+        zs_per_roof_on_tile=pd.concat([zs_per_roof_on_tile, zs_df_roughness], axis=1)
+        zs_per_roof_on_tile.rename(columns={'min': 'min_r', 'max': 'max_r', 'mean': 'mean_r', 'median': 'median_r',
+                                            'std': 'std_r', 'count': 'count_r'}, inplace=True)
 
-        roofs_high_variability=pd.concat([roofs_high_variability, zs_per_roof[(zs_per_roof['std']>LIM_STD) & (zs_per_roof['MOE']>LIM_MOE)]])
-        temp_roofs=zs_per_roof[~((zs_per_roof['MOE']>LIM_MOE) & (zs_per_roof['std']>LIM_STD))]
-
-        roofs_high_std=pd.concat([roofs_high_std, temp_roofs[temp_roofs['std']>LIM_STD]], ignore_index=True)
-        roofs_high_moe=pd.concat([roofs_high_moe, temp_roofs[temp_roofs['MOE']>LIM_MOE]], ignore_index=True)
-        zs_per_clear_roofs=pd.concat(
-            [zs_per_clear_roofs, temp_roofs[~((temp_roofs['MOE']>LIM_MOE) | (temp_roofs['std']>LIM_STD))]], 
-            ignore_index=True
-        )
-
+        zs_per_roof=pd.concat([zs_per_roof, zs_per_roof_on_tile], ignore_index=True)
+                              
     # else:
     #     logger.error(f'No raster found for the id {tile_id}.')
 
+logger.info('Checking for already occupied roofs...')
+
+# Check the marge or error
+zs_per_roof['MOE_i'] = Z*zs_per_roof['std_i']/(zs_per_roof['count_i']**(1/2))
+
+index_over_lim=[]
+for attribute in STAT_LIMITS.keys():
+    index_over_lim.extend(zs_per_roof.index[zs_per_roof[attribute] > STAT_LIMITS[attribute]].tolist())
+seen = set()
+dupes_index = [roof_index for roof_index in index_over_lim if roof_index in seen or seen.add(roof_index)]  
+
+roofs_high_variability=zs_per_roof[zs_per_roof.index.isin(dupes_index)]
+temp_roofs=zs_per_roof[~zs_per_roof.index.isin(dupes_index)]
+
+roofs_high_std=temp_roofs[temp_roofs['std_i']>LIM_STD]
+roofs_high_moe=temp_roofs[temp_roofs['MOE_i']>LIM_MOE]
+rough_roofs=temp_roofs[temp_roofs['median_r']>LIM_ROUGHNESS]
+zs_per_clear_roofs=temp_roofs[~((temp_roofs['MOE_i']>LIM_MOE) | (temp_roofs['std_i']>LIM_STD) | (temp_roofs['median_r']>LIM_ROUGHNESS))]
+
 roofs_high_variability=cause_occupation(
     roofs_high_variability,
-    f'The marge of error of the mean and the standard deviation are both over the limit, respectively {LIM_MOE} and {LIM_STD}.'
+    f'Several parameters are over the set limits.'
 )
 
-roofs_high_moe=cause_occupation(roofs_high_moe, f'The marge of error of the mean is over {LIM_MOE}.')
+roofs_high_moe=cause_occupation(roofs_high_moe, f'The marge of error of the mean for the intensity is over {LIM_MOE}.')
 
-roofs_high_std=cause_occupation(roofs_high_std, f'The standard deviation is over {LIM_STD}.')
+roofs_high_std=cause_occupation(roofs_high_std, f'The standard deviation for the intensity is over {LIM_STD}.')
 
-roofs_high_variability=pd.concat([roofs_high_variability, roofs_high_moe, roofs_high_std], ignore_index=True)
+rough_roofs=cause_occupation(rough_roofs, f'The median of the roughness is over {LIM_ROUGHNESS}.')
 
+roofs_high_variability=pd.concat([roofs_high_variability, roofs_high_moe, roofs_high_std, rough_roofs], ignore_index=True)
+
+zs_per_clear_roofs['status']='not occupied'
 
 # If roofs appear several times, keep the largest surface
 roofs_occupation=pd.concat([zs_per_clear_roofs, roofs_high_variability, small_roofs, other_classes_roofs, no_roofs], ignore_index=True)
@@ -215,6 +246,8 @@ logger.info(f'{roofs_occupation.shape[0]-roofs_occupation_cleaned.shape[0]} geom
 # Reattach to original geometries
 roofs_occupation_cleaned_df=pd.DataFrame(roofs_occupation_cleaned.drop(columns=['geometry', 'clipped_area']))
 roofs_occupation_cleaned_gdf=gpd.GeoDataFrame(roofs_occupation_cleaned_df.merge(roofs[['OBJECTID', 'geometry']], on='OBJECTID', how='left'), crs='EPSG:2056')
+roofs_occupation_cleaned_gdf=roofs_occupation_cleaned_gdf.round(3)
+roofs_occupation_cleaned_gdf.drop(columns=['max_i', 'min_i', 'mean_i', 'max_r', 'min_r', 'mean_r', 'count_r',])
 
 logger.info('Saving files...')
 filepath=os.path.join('processed', 'tests', 'roofs.gpkg')
