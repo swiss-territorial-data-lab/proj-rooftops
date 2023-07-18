ed869edec987d77fe454c00a83a909f3c04233e5
GwenaelleSa <gwenaelle.salamin@gmail.com>
2023-07-18 12:11:10 +0200

    Review of the tile generation

--- scripts/image_processing/tiles_generation.py
+++ scripts/image_processing/tiles_generation.py
@@ -1,7 +1,7 @@
 #!/bin/python
 # -*- coding: utf-8 -*-
 
-#  Proj rooftops
+#  Proj-rooftops
 #
 #      Clemence Herny 
 #      Gwenaelle Salamin
@@ -20,7 +20,6 @@ import geopandas as gpd
 import whitebox
 wbt = whitebox.WhiteboxTools()
 import rasterio as rio
-import rasterio.mask
 from rasterio.merge import merge
 
 sys.path.insert(1, 'scripts')
@@ -53,12 +52,14 @@ if __name__ == "__main__":
     # Load input parameters
     WORKING_DIR = cfg['working_dir']
     IMAGE_DIR = cfg['image_dir']
-    TILES_DIR = cfg['tiles_dir']
-    ROOFS_LIST = cfg['roofs_list_dir']
-    SHP_ROOFS = cfg['roofs_dir']
+    TILES_DIR = cfg['shp_tiles']
+    SHP_GT_EGID = cfg['shp_gt_egid']
+    SHP_ROOFS = cfg['shp_roofs']
     OUTPUT_DIR = cfg['output_dir']
     BUFFER = cfg['buffer']
     MASK = cfg['mask']
+    if MASK:
+        logger.info("Building mask will be applied")
 
     os.chdir(WORKING_DIR)
 
@@ -68,8 +69,8 @@ if __name__ == "__main__":
     written_files = []
 
     # Get the rooftops shapes
-    SHP_ROOFS, ROOFS_NAME = os.path.split(SHP_ROOFS)
-    feature_path = os.path.join(SHP_ROOFS, ROOFS_NAME[:-4]  + "_EGID.shp")
+    ROOFS_DIR, ROOFS_NAME = os.path.split(SHP_ROOFS)
+    feature_path = os.path.join(ROOFS_DIR, ROOFS_NAME[:-4]  + "_EGID.shp")
 
     if os.path.exists(feature_path):
         logger.info(f"File {ROOFS_NAME[:-4]}_EGID.shp already exists")
@@ -77,10 +78,10 @@ if __name__ == "__main__":
     else:
         logger.info(f"File {ROOFS_NAME[:-4]}_EGID.shp does not exist")
         logger.info(f"Create it")
-        gdf_roofs = gpd.read_file(WORKING_DIR  + '/' + SHP_ROOFS  + '/' + ROOFS_NAME)
+        gdf_roofs = gpd.read_file(os.path.join(WORKING_DIR, ROOFS_DIR, ROOFS_NAME))
+        gdf_roofs.drop(['OBJECTID', 'ALTI_MAX', 'DATE_LEVE', 'SHAPE_AREA', 'SHAPE_LEN'], axis=1, inplace=True)
         logger.info(f"Dissolved shapes by EGID number")
         rooftops = gdf_roofs.dissolve('EGID', as_index=False)
-        rooftops.drop(['OBJECTID', 'ALTI_MAX', 'DATE_LEVE', 'SHAPE_AREA', 'SHAPE_LEN'], axis=1)
         rooftops.to_file(feature_path)
         written_files.append(feature_path)  
         logger.info(f"...done. A file was written: {feature_path}")
@@ -95,20 +96,20 @@ if __name__ == "__main__":
     # rooftops_list = gpd.clip(rooftops, AOI)
 
     # # Get the EGID list from a file
-    egid_list = gpd.read_file(ROOFS_LIST)
-    rooftops_list = egid_list
+    egid_list_gdf = gpd.read_file(SHP_GT_EGID)
+    rooftops_list_gdf = egid_list_gdf.copy()
 
 
     # Find the image's tiles intersecting the rooftops' shapes  
-    logger.info("Intersection of rooftops' shape and images' tile")
-    join = gpd.sjoin(rooftops_list, tiles, how="left")
+    logger.info("Intersection of rooftop shapes and image tiles")
+    join_tiles_rooftops = gpd.sjoin(rooftops_list_gdf, tiles, how="left")
     
     image_list = []  
     egid_list = []
     coords_list = [] 
 
     # Open or produce the rooftops boundary boxes shapes 
-    feature_path = os.path.join(OUTPUT_DIR + 'bbox.gpkg')
+    feature_path = os.path.join(OUTPUT_DIR, 'bbox.gpkg')
 
     if os.path.exists(feature_path):
         logger.info(f"File bbox.gpkg already exists")
@@ -117,7 +118,7 @@ if __name__ == "__main__":
         logger.info(f"File bbox.gpkg does not exist")
         logger.info(f"Create it")
 
-        for row in join.itertuples():
+        for row in join_tiles_rooftops.itertuples():
             egid = row.EGID
             egid_list.append(egid)
             bounds = row.geometry.bounds
@@ -132,58 +133,52 @@ if __name__ == "__main__":
         logger.info(f"...done. A file was written: {feature_path}")
 
     # Get the image tile(s) number intersecting the rooftop shape 
-    logger.info("Find the image tile(s) number(s) intersecting the rooftop shape")
-    unique_egid = join["EGID"].unique() 
+    logger.info("Find the number of image tile(s) intersecting the rooftop shape")
+    unique_egid = join_tiles_rooftops["EGID"].unique() 
 
-    for i in tqdm(unique_egid[:], desc='EGID in AOI', total=len(unique_egid)):
-        tiles_list = join[join['EGID'] == i]
-        image_list = ((tiles_list['TileName'].to_numpy()).tolist())
+    for egid in tqdm(unique_egid, desc='Make adapted tiles for EGIDs in AOI', total=len(unique_egid)):
+        image_list = join_tiles_rooftops.loc[join_tiles_rooftops['EGID'] == egid, 'TileName'].to_numpy().tolist()
 
-        tiles_list = [] 
-        for image in image_list:
-            tiles_list.append(os.path.join(IMAGE_DIR, image + '.tif'))            
+        tiles_list = [os.path.join(IMAGE_DIR, image + '.tif') for image in image_list]   
 
-            raster_to_mosaic = []   
-            for tile in tqdm(tiles_list, desc='Applying SAM to tiles', total=len(tiles_list)):
-                raster = rio.open(tile)
-                
-                # Mosaic images if rooftops shape is spread over several tiles 
-                if len(tiles_list) > 1:  
-                    logger.info("Mosaic production")
-                    raster_to_mosaic.append(raster)
-                    mosaic, output = merge(raster_to_mosaic)
+        raster_to_mosaic = []   
+        for tilepath in tiles_list:
+            raster = rio.open(tilepath)
 
-                    output_meta = raster.meta.copy()
-                    output_meta.update(
-                    {"driver": "GTiff",
-                        "height": mosaic.shape[1],
-                        "width": mosaic.shape[2],
-                        "transform": output,
-                    })
+            # Mosaic images if rooftops shape is spread over several tiles 
+            if len(tiles_list) > 1:  
+                raster_to_mosaic.append(raster)
 
-                    mosaic_path = os.path.join(OUTPUT_DIR, 'mosaic.tif')
-                    with rio.open(mosaic_path, "w", **output_meta) as m:
-                        m.write(mosaic)
+        if len(tiles_list) > 1:
+            logger.info(f"Mosaic production for EGID {egid}")
+            mosaic, output = merge(raster_to_mosaic)
 
-                    written_files.append(mosaic_path)  
-                    logger.info(f"...done. A file was written: {mosaic_path}")
+            output_meta = raster.meta.copy()
+            output_meta.update(
+            {"driver": "GTiff",
+                "height": mosaic.shape[1],
+                "width": mosaic.shape[2],
+                "transform": output
+            })
+            
+            mosaic_path = os.path.join(OUTPUT_DIR, 'mosaic.tif')
+            with rio.open(mosaic_path, "w", **output_meta) as dst_mosaic:
+                dst_mosaic.write(mosaic)
 
-                    raster = rio.open(mosaic_path)
-                    tile = mosaic_path
+            raster = rio.open(mosaic_path)
+            tile = mosaic_path
 
-                image = raster
+        image = raster
 
         if MASK:
-            logger.info("Apply building mask")
-            egid_shape = rooftops[rooftops['EGID'] == i]['geometry'].buffer(BUFFER, join_style=2)                
-                
-            with rio.open(tile) as src:
-                mask_image, mask_transform = rio.mask.mask(src, egid_shape)
-                mask_meta=src.meta
+            egid_shape = rooftops.loc[rooftops['EGID'] == egid, 'geometry'].buffer(BUFFER, join_style=2)                
+
+            mask_image, mask_transform = rio.mask.mask(raster, egid_shape)
+            mask_meta=raster.meta
 
             mask_meta.update({'transform': mask_transform})
             feature_mask_path=os.path.join(fct_misc.ensure_dir_exists(os.path.join(OUTPUT_DIR, 'masked_images')),
-                                     f"tile_EGID_{int(i)}_masked.tif")
+                                    f"tile_EGID_{int(egid)}_masked.tif")
                 
             with rio.open(feature_mask_path, 'w', **mask_meta) as dst:
                 dst.write(mask_image)
@@ -191,33 +186,34 @@ if __name__ == "__main__":
 
             image = raster
 
-        logger.info("Rooftops' boundary box shapes")
-        bbox_shape = bbox_list[bbox_list['EGID'] == i]['geometry'].buffer(BUFFER, join_style=2)  
+        bbox_shape = bbox_list.loc[bbox_list['EGID'] == egid, 'geometry'].buffer(BUFFER, join_style=2)  
 
         # Clip image by the rooftops bounding box shape
-        logger.info("Clip image with bounding box")
         out_image, out_transform = rio.mask.mask(image, bbox_shape, crop = True)
         out_meta = image.meta
         out_meta.update({"driver": "GTiff",
                         "height": out_image.shape[1],
                         "width": out_image.shape[2],
-                        "transform": out_transform})
+                        "transform": out_transform,
+                        "crs": rio.CRS.from_epsg(2056)})
 
         if MASK:
            feature_path = feature_mask_path
         else: 
-            feature_path = os.path.join(OUTPUT_DIR, f"tile_EGID_{int(i)}.tif")
-        with rio.open(feature_path, "w", **out_meta) as dest:
-            dest.write(out_image)
+            feature_path = os.path.join(OUTPUT_DIR, f"tile_EGID_{int(egid)}.tif")
+        
+        # Close readers so we can re-write, erase file
+        del raster, image
+        with rio.open(feature_path, "w", **out_meta) as dst:
+            dst.write(out_image)
         written_files.append(feature_path)  
-        logger.info(f"...done. A file was written: {feature_path}")
 
         # Delete the produced image mosaic 
         try:
             if os.path.exists(mosaic_path):
                 os.remove(mosaic_path)
         except NameError:
-            print()
+            pass
 
     print()
     logger.info("The following files were written. Let's check them out!")
