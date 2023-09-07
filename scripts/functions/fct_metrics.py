149e42b1c856bfa4a8e0613b50f49a48290dbd60
Clemence Herny <clemence.herny@gmail.com>
2023-09-07 10:31:13 +0000

    Add computation of Jaccard index by EGID

--- scripts/functions/fct_metrics.py
+++ scripts/functions/fct_metrics.py
@@ -37,20 +37,20 @@ def apply_iou_threshold_one_to_one(tp_gdf_ini, threshold=0):
 
     # Filter detection based on IOU value
     # Keep only max IOU value for each detection
-    tp_grouped_gdf = tp_gdf_ini.groupby(['ID_DET'], group_keys=False).apply(lambda g:g[g.IOU==g.IOU.max()])
+    tp_grouped_gdf = tp_gdf_ini.groupby(['detection_id'], group_keys=False).apply(lambda g:g[g.IOU==g.IOU.max()])
     
     # Detection with IOU lower than threshold value are considered as FP and removed from TP list   
     fp_gdf_temp = tp_grouped_gdf[tp_grouped_gdf['IOU'] < threshold]
-    id_det_fp = fp_gdf_temp['ID_DET'].unique().tolist()
-    tp_gdf_temp = tp_grouped_gdf[~tp_grouped_gdf['ID_DET'].isin(id_det_fp)]
+    detection_id_fp = fp_gdf_temp['detection_id'].unique().tolist()
+    tp_gdf_temp = tp_grouped_gdf[~tp_grouped_gdf['detection_id'].isin(detection_id_fp)]
 
     # For each label, only keep the pred with the best IOU.
     sorted_tp_gdf_temp = tp_gdf_temp.sort_values(by='IOU')
-    tp_gdf=sorted_tp_gdf_temp.drop_duplicates(['ID_GT'], keep='last', ignore_index=True)
+    tp_gdf = sorted_tp_gdf_temp.drop_duplicates(['label_id'], keep='last', ignore_index=True)
 
     # Save the dropped preds due to multiple detections of the same label.
-    id_det_not_dropped = tp_gdf.ID_DET.unique()
-    fp_gdf_temp = pd.concat([fp_gdf_temp, sorted_tp_gdf_temp[~sorted_tp_gdf_temp.ID_DET.isin(id_det_not_dropped)]], ignore_index=True)
+    detection_id_not_dropped = tp_gdf.detection_id.unique()
+    fp_gdf_temp = pd.concat([fp_gdf_temp, sorted_tp_gdf_temp[~sorted_tp_gdf_temp.detection_id.isin(detection_id_not_dropped)]], ignore_index=True)
 
     return tp_gdf, fp_gdf_temp
 
@@ -68,31 +68,31 @@ def apply_iou_threshold_one_to_many(tp_gdf_ini, threshold=0):
     """
     
     # Compare the global IOU of the detection based on all the matching labels
-    sum_detections_gdf=tp_gdf_ini.groupby(['ID_DET'])['IOU'].sum().reset_index()
+    sum_detections_gdf=tp_gdf_ini.groupby(['detection_id'])['IOU'].sum().reset_index()
     true_detections_gdf=sum_detections_gdf[sum_detections_gdf['IOU']>threshold]
-    true_detections_index=true_detections_gdf['ID_DET'].unique().tolist()
+    true_detections_index=true_detections_gdf['detection_id'].unique().tolist()
 
     # Check that the label is at least 25% under the prediction.
     tp_gdf_ini['label_in_pred']=round(tp_gdf_ini['geom_GT'].intersection(tp_gdf_ini['geom_DET']).area/tp_gdf_ini['geom_GT'].area, 3)
-    tp_gdf_temp=tp_gdf_ini[(tp_gdf_ini['ID_DET'].isin(true_detections_index)) & (tp_gdf_ini['label_in_pred'] > 0.25)]
+    tp_gdf_temp=tp_gdf_ini[(tp_gdf_ini['detection_id'].isin(true_detections_index)) & (tp_gdf_ini['label_in_pred'] > 0.25)]
 
     # For each label, only keep the pred with the best IOU.
     sorted_tp_gdf_temp = tp_gdf_temp.sort_values(by='IOU')
-    tp_gdf=sorted_tp_gdf_temp.drop_duplicates(['ID_GT'], keep='last', ignore_index=True)
-    id_det_tp=tp_gdf['ID_DET'].unique().tolist()
+    tp_gdf=sorted_tp_gdf_temp.drop_duplicates(['label_id'], keep='last', ignore_index=True)
+    detection_id_tp=tp_gdf['detection_id'].unique().tolist()
 
-    fp_gdf_temp=tp_gdf_ini[~tp_gdf_ini['ID_DET'].isin(id_det_tp)]
-    fp_gdf_temp = fp_gdf_temp.groupby(['ID_DET'], group_keys=False).apply(lambda g:g[g.IOU == g.IOU.max()])
+    fp_gdf_temp=tp_gdf_ini[~tp_gdf_ini['detection_id'].isin(detection_id_tp)]
+    fp_gdf_temp = fp_gdf_temp.groupby(['detection_id'], group_keys=False).apply(lambda g:g[g.IOU == g.IOU.max()])
 
     return tp_gdf, fp_gdf_temp
 
 
-def get_fractional_sets(preds_gdf, labels_gdf, method='one-to-one'):
+def get_fractional_sets(detections_gdf, labels_gdf, method='one-to-one'):
     """Separate the predictions and labels between TP, FP and FN based on their overlap and the passed IoU score.
     One prediction can either correspond to one (one-to-one) or several (one-to-many) labels.
 
     Args:
-        preds_gdf (geodataframe): geodataframe of the prediction with the id "ID_DET"
+        detections_gdf (geodataframe): geodataframe of the detection with the id "detection_idection"
         labels_gdf (geodataframe): threshold to apply on the IoU to determine TP and FP
         method (str, optional): string with the possible values 'one-to-one' or 'one-to-many' indicating if a prediction can or not correspond to several labels. 
                 Defaults to 'one-to-one'.
@@ -105,57 +105,57 @@ def get_fractional_sets(preds_gdf, labels_gdf, method='one-to-one'):
     """
 
     if len(labels_gdf) == 0:
-        fp_gdf = preds_gdf.copy()
+        fp_gdf = detections_gdf.copy()
         tp_gdf = gpd.GeoDataFrame()
         fn_gdf = gpd.GeoDataFrame()       
         return tp_gdf, fp_gdf, fn_gdf
     
     try:
-        assert(preds_gdf.crs == labels_gdf.crs), f"CRS mismatch: predictions' CRS = {preds_gdf.crs}, labels' CRS = {labels_gdf.crs}"
+        assert(detections_gdf.crs == labels_gdf.crs), f"CRS mismatch: predictions' CRS = {detections_gdf.crs}, labels' CRS = {labels_gdf.crs}"
     except Exception as e:
         raise Exception(e)
 
     # CREATE ADDITIONAL COLUMN FOR TP, FP AND FN CLASSIFICATION AND IOU COMPUTATION
-    labels_gdf['geom_GT'] = labels_gdf.geometry
-    preds_gdf['geom_DET'] = preds_gdf.geometry
+    labels_gdf['label_geometry'] = labels_gdf.geometry
+    detections_gdf['detection_geometry'] = detections_gdf.geometry
 
     # TRUE POSITIVES
-    left_join = gpd.sjoin(preds_gdf, labels_gdf, how='left', predicate='intersects', lsuffix='left', rsuffix='right')
-    tp_gdf_temp = left_join[left_join.ID_GT.notnull()].copy()
+    left_join = gpd.sjoin(detections_gdf, labels_gdf, how='left', predicate='intersects', lsuffix='left', rsuffix='right')
+    tp_gdf_temp = left_join[left_join.label_geometry.notnull()].copy()
 
-    # IOU computation between GT geometry and Detection geometry
-    geom1 = tp_gdf_temp['geom_DET'].to_numpy().tolist()
-    geom2 = tp_gdf_temp['geom_GT'].to_numpy().tolist()
+    # IOU computation between label geometry and detection geometry
+    geom1 = tp_gdf_temp['label_geometry'].to_numpy().tolist()
+    geom2 = tp_gdf_temp['detection_geometry'].to_numpy().tolist()
     iou = []
     for (i, ii) in zip(geom1, geom2):
         iou.append(intersection_over_union(i, ii))
     tp_gdf_temp['IOU'] = iou
 
-    iou_threshold=0.1
-    if method=='one-to-many':
+    iou_threshold = 0.1
+    if method == 'one-to-many':
         tp_gdf, fp_gdf_temp = apply_iou_threshold_one_to_many(tp_gdf_temp, iou_threshold)
     else:
         tp_gdf, fp_gdf_temp = apply_iou_threshold_one_to_one(tp_gdf_temp, iou_threshold)
 
 
     # FALSE POSITIVES -> potentially object not referenced in ground truth or mistakes
-    fp_gdf = left_join[left_join.ID_GT.isna()].copy()
+    fp_gdf = left_join[left_join.label_geometry.isna()].copy()
     fp_gdf = pd.concat([fp_gdf, fp_gdf_temp])
     assert(len(fp_gdf[fp_gdf.duplicated()]) == 0)
 
 
     # FALSE NEGATIVES -> objects that have been missed by the detection algorithm
-    right_join = gpd.sjoin(labels_gdf, preds_gdf, how='left', predicate='intersects', lsuffix='left', rsuffix='right')
+    right_join = gpd.sjoin(labels_gdf, detections_gdf, how='left', predicate='intersects', lsuffix='left', rsuffix='right')
     
-    id_gt_tp=tp_gdf['ID_GT'].unique().tolist()
-    suppressed_tp=tp_gdf_temp[~tp_gdf_temp['ID_GT'].isin(id_gt_tp)]
-    id_gt_filter = suppressed_tp['ID_GT'].unique().tolist()
+    id_label_tp = tp_gdf['label_id'].unique().tolist()
+    suppressed_tp = tp_gdf_temp[~tp_gdf_temp['label_id'].isin(id_label_tp)]
+    id_label_filter = suppressed_tp['label_id'].unique().tolist()
     
-    fn_too_low_hit_gdf = right_join[right_join['ID_GT'].isin(id_gt_filter)]
-    fn_no_hit_gdf = right_join[right_join.ID_DET.isna()].copy()
+    fn_too_low_hit_gdf = right_join[right_join['label_id'].isin(id_label_filter)]
+    fn_no_hit_gdf = right_join[right_join.detection_id.isna()].copy()
     fn_gdf = pd.concat([fn_no_hit_gdf, fn_too_low_hit_gdf])
    
-    fn_gdf.drop_duplicates(subset=['ID_GT'], inplace=True)
+    fn_gdf.drop_duplicates(subset=['label_id'], inplace=True)
 
     # Tag predictions   
     tp_gdf['tag'] = 'TP'
@@ -163,6 +163,31 @@ def get_fractional_sets(preds_gdf, labels_gdf, method='one-to-one'):
     fn_gdf['tag'] = 'FN'
 
     return tp_gdf, fp_gdf, fn_gdf
+
+
+def get_jaccard_index_roof(labels_gdf, detections_gdf):
+    """Compute the IoU (Jaccard index) of all the detection by roof (EGID)
+
+    Args:
+        detections_gdf (geodataframe): geodataframe of the detection with "detection_geometry" and "EGID" columns
+        labels_gdf (geodataframe): geodataframe of the ground truth with "detection_geometry" and "EGID" columns
+
+    Returns:
+        labels_egid_gdf: geodataframes of all the labels merged by roof
+        detections_egid_gdf: geodataframes of all the detections merged by roof and with the IoU by roof
+    """
+
+    detections_egid_gdf = detections_gdf.dissolve(by='EGID', as_index=False) 
+    labels_egid_gdf = labels_gdf.dissolve(by='EGID', as_index=False) 
+
+    geom1 = detections_egid_gdf.geometry.values.tolist()
+    geom2 = labels_egid_gdf.geometry.values.tolist()
+    iou = []
+    for (i, ii) in zip(geom1, geom2):
+        iou.append(intersection_over_union(i, ii))
+    detections_egid_gdf['IOU_EGID'] = iou
+
+    return labels_egid_gdf, detections_egid_gdf
 
 
 def get_metrics(tp_gdf, fp_gdf, fn_gdf):
--- scripts/image_processing/assess_results.py
+++ scripts/image_processing/assess_results.py
@@ -27,14 +27,14 @@ logger = fct_misc.format_logger(logger)
 
 # Define functions --------------------------
 
-def main(WORKING_DIR, OUTPUT_DIR, DETECTIONS, GT, EGIDS, METHOD):
+def main(WORKING_DIR, OUTPUT_DIR, DETECTIONS, LABELS, EGIDS, METHOD):
     """Assess the results by calculating the precision, recall and f1-score.
 
     Args:
         WORKING_DIR (path): working directory
         OUTPUT_DIR (path): output directory
         DETECTIONS (path): file of the detections
-        GT (path): file of the ground truth
+        LABELS (path): file of the ground truth
         EGIDS (list): EGIDs of interest
         METHOD (string): method to use for the assessment of the results, either one-to-one or one-to-many.
 
@@ -45,58 +45,59 @@ def main(WORKING_DIR, OUTPUT_DIR, DETECTIONS, GT, EGIDS, METHOD):
     os.chdir(WORKING_DIR)
 
     # Create an output directory in case it doesn't exist
-    _ = fct_misc.ensure_dir_exists(OUTPUT_DIR)
+    fct_misc.ensure_dir_exists(OUTPUT_DIR)
 
-    written_files={}
+    written_files = {}
 
     # Get the EGIDS of interest
     egids = pd.read_csv(EGIDS)
+
     # Open shapefiles
-    gdf_gt = gpd.read_file(GT)
-    # if 'OBSTACLE' in gdf_gt.columns:
-    #     gdf_gt.rename(columns={'OBSTACLE': 'occupation'}, inplace=True)
+    labels_gdf = gpd.read_file(LABELS)
+    # if 'OBSTACLE' in labels_gdf.columns:
+    #     labels_gdf.rename(columns={'OBSTACLE': 'occupation'}, inplace=True)
 
-    gdf_gt['fid'] = gdf_gt['fid'].astype(int)
-    gdf_gt['type'] = gdf_gt['type'].astype(int)
-    # gdf_gt['egid_new'] = gdf_gt['egid_new'].astype(int)
+    labels_gdf['fid'] = labels_gdf['fid'].astype(int)
+    labels_gdf['class'] = labels_gdf['class'].astype(int)
+    labels_gdf['EGID'] = labels_gdf['EGID'].astype(int)
 
-    # Class 12 correspond to free surfaces, other classes are ojects
-    gdf_gt = gdf_gt[(gdf_gt.type != 12) & (gdf_gt.egid.isin(egids.EGID.to_numpy()))]
-    gdf_gt['ID_GT'] = gdf_gt.index
+    # Class 12 corresponds to free surfaces, other classes are ojects
+    labels_gdf = labels_gdf[(labels_gdf.type != 12) & (labels_gdf.EGID.isin(egids.EGID.to_numpy()))]
+    labels_gdf['label_id'] = labels_gdf.index
 
-    gdf_gt = gdf_gt.rename(columns={"area": "area_GT", 'EGID': 'egid_new'})
-    nbr_labels = gdf_gt.shape[0]
-    logger.info(f"Read GT file: {nbr_labels} shapes")
+    nbr_labels = labels_gdf.shape[0]
+    logger.info(f"Read LABELS file: {nbr_labels} shapes")
 
     if isinstance(DETECTIONS, str):
-        # gdf_detec = gpd.read_file(DETECTIONS, layer='occupation_for_all_EGIDS')
-        # gdf_detec = gpd.read_file(DETECTIONS, layer='EGID_occupation')
-        gdf_detec = gpd.read_file(DETECTIONS)
+        # detections_gdf = gpd.read_file(DETECTIONS, layer='occupation_for_all_EGIDS')
+        # detections_gdf = gpd.read_file(DETECTIONS, layer='EGID_occupation')
+        detections_gdf = gpd.read_file(DETECTIONS)
     elif isinstance(DETECTIONS, gpd.GeoDataFrame):
-        gdf_detec = DETECTIONS
+        detections_gdf = DETECTIONS
     else:
         logger.critical(f'Unrecognized variable type for the detections: {type(DETECTIONS)}')
-    # gdf_detec = gdf_detec[gdf_detec['occupation'].astype(int) == 1]
 
+    detections_gdf['EGID'] = detections_gdf['EGID'].astype(int)
+    if 'value' in detections_gdf.columns:
+         detections_gdf.rename(columns={'value': 'detection_id'}, inplace=True)
+    detections_gdf['detection_id'] = detections_gdf['detection_id'].astype(int)
+    detections_gdf = detections_gdf.rename(columns={"area": "detection_area"})
 
-    if 'value' in gdf_detec.columns:
-        gdf_detec.rename(columns={'value': 'pred_id'}, inplace=True)
-    gdf_detec['ID_DET'] = gdf_detec.pred_id
-    gdf_detec = gdf_detec.rename(columns={"area": "area_DET"})
-    logger.info(f"Read detection file: {len(gdf_detec)} shapes")
+    logger.info(f"Read detection file: {len(detections_gdf)} shapes")
 
-    logger.info(f"Metrics computation")
+    # Detections count
+    logger.info(f"Method used for detections counting")
     if METHOD == 'one-to-one':
         logger.info('Using the one-to-one method.')
     elif METHOD == 'one-to-many':
         logger.info('Using one-to-many method.')
     else:
-        logger.warning('Unknown method, defaulting to one-to-one.')
+        logger.warning('Unknown method, default one-to-one.')
 
     logger.info(f"Metrics computation:")
-    logger.info(f" - Compute TP, FP and FN")
+    logger.info(f" - Count TP, FP and FN")
 
-    tp_gdf, fp_gdf, fn_gdf = metrics.get_fractional_sets(gdf_detec, gdf_gt, method=METHOD)
+    tp_gdf, fp_gdf, fn_gdf = metrics.get_fractional_sets(detections_gdf, labels_gdf, method=METHOD)
 
     # Compute metrics
     precision, recall, f1 = metrics.get_metrics(tp_gdf, fp_gdf, fn_gdf)
@@ -107,10 +108,10 @@ def main(WORKING_DIR, OUTPUT_DIR, DETECTIONS, GT, EGIDS, METHOD):
 
     if METHOD == 'one-to-many':
         tp_with_duplicates = tp_gdf.copy()
-        dissolved_tp_gdf = tp_with_duplicates.dissolve(by=['ID_DET'], as_index=False)
+        dissolved_tp_gdf = tp_with_duplicates.dissolve(by=['detection_id'], as_index=False)
 
         geom1 = dissolved_tp_gdf.geometry.values.tolist()
-        geom2 = dissolved_tp_gdf['geom_GT'].values.tolist()
+        geom2 = dissolved_tp_gdf['label_geometry'].values.tolist()
         iou = []
         for (i, ii) in zip(geom1, geom2):
             iou.append(metrics.intersection_over_union(i, ii))
@@ -118,16 +119,18 @@ def main(WORKING_DIR, OUTPUT_DIR, DETECTIONS, GT, EGIDS, METHOD):
 
         tp_gdf = dissolved_tp_gdf.copy()
 
-        logger.info(f'{tp_with_duplicates.shape[0]-tp_gdf.shape[0]} labels are under a shared predictions with at least one other label.')
+        logger.info(f'{tp_with_duplicates.shape[0] - tp_gdf.shape[0]} labels are under a shared predictions with at least one other label.')
 
     logger.info(f"   TP = {TP}, FP = {FP}, FN = {FN}")
     logger.info(f"   precision = {precision:.2f}, recall = {recall:.2f}, f1 = {f1:.2f}")
     logger.info(f" - Compute mean Jaccard index")
-    if TP!=0:
-        iou_average = tp_gdf['IOU'].mean()
-        logger.info(f"   IOU average = {iou_average:.2f}")
         
+    # Compute Jaccard index at the scale of a roof (by EGID)
+    labels_egid_gdf, detections_egid_gdf = metrics.get_jaccard_index_roof(labels_gdf, detections_gdf)
+    iou_average = detections_egid_gdf['IOU_EGID'].mean()
+    logger.info(f"   averaged IOU for all EGIDs = {iou_average:.2f}")
 
+    # Check if detection or labels have been lost in the process
     nbr_tagged_labels = TP + FN
     diff_in_labels = nbr_labels - nbr_tagged_labels
     filename = os.path.join(OUTPUT_DIR, 'problematic_objects.gpkg')
@@ -138,20 +141,20 @@ def main(WORKING_DIR, OUTPUT_DIR, DETECTIONS, GT, EGIDS, METHOD):
         logger.info(f'The list of the problematic labels in exported to {filename}.')
 
         if diff_in_labels > 0:
-            tagged_labels = tp_gdf['ID_GT'].unique().tolist() + fn_gdf['ID_GT'].unique().tolist()
+            tagged_labels = tp_gdf['label_id'].unique().tolist() + fn_gdf['label_id'].unique().tolist()
 
-            untagged_gt_gdf = gdf_gt[~gdf_gt['ID_GT'].isin(tagged_labels)]
-            untagged_gt_gdf.drop(columns=['geom_GT', 'OBSTACLE'], inplace=True)
+            untagged_labels_gdf = labels_gdf[~labels_gdf['label_id'].isin(tagged_labels)]
+            untagged_labels_gdf.drop(columns=['label_geometry'], inplace=True)
 
             layer_name = 'missing_label_tags'
-            untagged_gt_gdf.to_file(filename, layer=layer_name, index=False)
+            untagged_labels_gdf.to_file(filename, layer=layer_name, index=False)
 
         elif diff_in_labels < 0:
             all_tagged_labels_gdf=pd.concat([tp_gdf, fn_gdf])
 
-            duplicated_id_gt = all_tagged_labels_gdf.loc[all_tagged_labels_gdf.duplicated(subset=['ID_GT']), 'ID_GT'].unique().tolist()
-            duplicated_labels = all_tagged_labels_gdf[all_tagged_labels_gdf['ID_GT'].isin(duplicated_id_gt)]
-            duplicated_labels.drop(columns=['geom_GT', 'geom_DET', 'index_right', 'EGID', 'occupation_left', 'occupation_right'], inplace=True)
+            duplicated_label_id = all_tagged_labels_gdf.loc[all_tagged_labels_gdf.duplicated(subset=['label_id']), 'label_id'].unique().tolist()
+            duplicated_labels = all_tagged_labels_gdf[all_tagged_labels_gdf['label_id'].isin(duplicated_label_id)]
+            duplicated_labels.drop(columns=['label_geometry', 'detection_geometry', 'index_right', 'EGID', 'occupation_left', 'occupation_right'], inplace=True)
 
             layer_name = 'duplicated_label_tags'
             duplicated_labels.to_file(filename, layer=layer_name, index=False)
@@ -160,19 +163,15 @@ def main(WORKING_DIR, OUTPUT_DIR, DETECTIONS, GT, EGIDS, METHOD):
 
 
     # Set the final dataframe with tagged prediction
-
     tagged_preds_gdf = pd.concat([tp_gdf, fp_gdf, fn_gdf])
-    tagged_preds_gdf.drop(['fid', 'index_right', 'geom_GT', 'geom_DET', 'ID_DET', 'area_DET', 'egid_new', 'egid'], axis=1, inplace=True)
-    # tagged_preds_gdf.drop(['index_right', 'occupation_left', 'occupation_right', 'geom_GT', 'geom_DET', 'ID_DET', 'area_DET'], axis = 1, inplace=True)
+    tagged_preds_gdf.drop(['index_right', 'label_geometry', 'detection_geometry', 'detection_id', 'detection_area'], axis=1, inplace=True)
     tagged_preds_gdf = tagged_preds_gdf.round({'IOU': 2})
-    
     tagged_preds_gdf.reset_index(drop=True, inplace=True)
     tagged_preds_gdf['fid'] = tagged_preds_gdf.index
 
     layer_name = 'tagged_predictions'
     feature_path = os.path.join(OUTPUT_DIR, 'tagged_predictions.gpkg')
-    tagged_preds_gdf.to_file(feature_path, index=False)
-    # tagged_preds_gdf.to_file(feature_path, layer=layer_name, index=False)
+    tagged_preds_gdf.to_file(feature_path, layer=layer_name, index=False)
 
     written_files[feature_path] = layer_name
 
@@ -205,11 +204,11 @@ if __name__ == "__main__":
     OUTPUT_DIR = cfg['output_dir']
 
     DETECTIONS=cfg['detections']
-    GT = cfg['gt']
+    LABELS = cfg['ground_truth']
     EGIDS = cfg['egids']
     METHOD = cfg['method']
 
-    f1, diff_in_labels = main(WORKING_DIR, OUTPUT_DIR, DETECTIONS, GT, EGIDS, METHOD)
+    f1, diff_in_labels = main(WORKING_DIR, OUTPUT_DIR, DETECTIONS, LABELS, EGIDS, METHOD)
 
     # Stop chronometer  
     toc = time.time()
