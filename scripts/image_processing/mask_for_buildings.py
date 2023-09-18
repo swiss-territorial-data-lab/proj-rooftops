3fecfeb5418482fd0b6bb973e0e1a8fe2259f6da
GwenaelleSa <gwenaelle.salamin@gmail.com>
2023-09-18 12:58:06 +0200

    Add missing parameter

--- scripts/image_processing/mask_for_buildings.py
+++ scripts/image_processing/mask_for_buildings.py
@@ -29,7 +29,7 @@ IMAGE_FOLDER = cfg['image_folder']
 
 os.chdir(WORKING_DIR)
 
-misc.ensure_dir_exists('processed/tiles/mask')
+output_dir = misc.ensure_dir_exists('processed/tiles/mask')
 
 logger.info('Loading data...')
 roofs = gpd.read_file(ROOFS)
