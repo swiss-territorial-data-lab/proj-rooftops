1326ddabc35ba86f45e48f15e5816cf9c5c45362
Clemence Herny <clemence.herny@gmail.com>
2023-10-20 09:38:08 +0000

    Set GT vector as roof shape in script produce_vector_layer.py

--- scripts/image_processing/produce_vector_layer.py
+++ scripts/image_processing/produce_vector_layer.py
@@ -23,7 +23,8 @@ import functions.fct_misc as misc
 
 logger = misc.format_logger(logger)
 
-def main(WORKING_DIR, EGIDS, ROOFS, OUTPUT_DIR, SHP_EXT, SRS):
+
+def main(WORKING_DIR, EGIDS, ROOFS, OUTPUT_DIR, SHP_EXT, CRS):
 
     os.chdir(WORKING_DIR)
 
@@ -44,64 +45,61 @@ def main(WORKING_DIR, EGIDS, ROOFS, OUTPUT_DIR, SHP_EXT, SRS):
     attribute = 'EGID'
     original_file_path = os.path.join(ROOFS_DIR, ROOFS_NAME)
     desired_file_path = os.path.join(ROOFS_DIR, ROOFS_NAME[:-4]  + "_" + attribute + ".shp")
-    
     roofs = misc.dissolve_by_attribute(desired_file_path, original_file_path, name=ROOFS_NAME[:-4], attribute=attribute)
+    roofs['EGID'] = roofs['EGID'].astype(int)
     roofs_gdf = roofs[roofs.EGID.isin(egids.EGID.to_numpy())]
-    roofs_gdf['EGID'] = roofs_gdf['EGID'].astype(int)
 
     logger.info(f"  Number of building to process: {len(roofs_gdf)}")
 
 
-    # Read all the shapefile produced, filter them with rooftop extension and merge them in a single layer  
+    # Read all the shapefiles produced, filter them and merge them in a single layer  
     logger.info(f"Read shapefiles' name")
     tiles = glob(os.path.join(detection_dir, '*.' + SHP_EXT))
     vector_layer = gpd.GeoDataFrame() 
 
     for tile in tqdm(tiles, desc='Read detection shapefiles', total=len(tiles)):
-    
+
+        # Prepare objects shp 
         objects = gpd.read_file(tile)
+        objects_shp = objects.copy()
+        objects_shp.crs = CRS
+        objects_shp['area_shp'] = objects_shp.area 
+        objects_shp['geometry_shp'] = objects_shp.geometry
 
-        # Set CRS
-        objects.crs = SRS
-
-        # object_shp = objects.explode(index_parts=True)
-        object_shp = objects
-        object_shp['area_shp'] = object_shp.area 
-        object_shp['geometry_shp'] = object_shp.geometry
-        # object_shp = objects.dissolve('mask_value', as_index=False)
+        # Prepare roofs shp
         egid = float(re.sub('[^0-9]','', os.path.basename(tile)))
-
-        egid_shp = roofs[roofs['EGID'] == egid]
-        # egid_shp = egid_shp.explode(index_parts=True)
+        egid_shp = roofs_gdf[roofs_gdf['EGID'] == egid]
         egid_shp['area_roof'] = egid_shp.area
         egid_shp['geometry_roof'] = egid_shp.geometry
-        egid_shp.geometry = egid_shp.geometry.buffer(1.0)
+        roof_buffer = 1
+        egid_shp.geometry = egid_shp.geometry.buffer(roof_buffer)
 
-        misc.test_crs(object_shp, egid_shp)
+        misc.test_crs(objects_shp, egid_shp)
 
-        object_selection = object_shp.sjoin(egid_shp, how='inner', predicate="within")
+        # Filter vectorised objects. Threshold values have been set
+        objects_selection = objects_shp.sjoin(egid_shp, how='inner', predicate="within")
+        objects_selection['intersection_frac'] = objects_selection['geometry_roof'].intersection(objects_selection['geometry_shp']).area / objects_selection['area_shp']
+        objects_filtered = objects_selection[(objects_selection['area_shp'] >= 0.1) &
+                                            (objects_selection['area_shp'] <= 0.8 * objects_selection['area_roof']) &
+                                            (objects_selection['intersection_frac'] >= 0.5)]
 
-        object_selection['intersection_frac'] = object_selection['geometry_roof'].intersection(object_selection['geometry_shp']).area / object_selection['area_shp']
-        object_filtered = object_selection[(object_selection['area_shp'] >= 0.1) &
-                                            (object_selection['area_shp'] <= 0.8 * object_selection['area_roof']) &
-                                            (object_selection['intersection_frac'] >= 0.5)]
+        objects_filtered['area'] = objects_filtered.area 
 
-        
-        object_filtered['area'] = object_filtered.area 
-        final_gdf = object_filtered.drop(['geometry_shp', 'geometry_roof'], axis=1)
-        final_gdf = gpd.clip(final_gdf, egid_shp)
+        objects_filtered = objects_filtered.drop(['geometry_shp', 'geometry_roof'], axis=1)
+        objects_clip = gpd.clip(objects_filtered, egid_shp.geometry.buffer(-roof_buffer))
         
         # Merge/Combine multiple shapefiles into one gdf
-        vector_layer = gpd.pd.concat([vector_layer, final_gdf], ignore_index=True)
+        vector_layer = gpd.pd.concat([vector_layer, objects_clip], ignore_index=True)
 
-    # Save the vectors for each EGID in layers in a gpkg !!! Will be deleted at the end !!!
-    feature_path = os.path.join(output_dir, "roof_segmentation_egid.gpkg")
-    for egid in vector_layer.EGID.unique():
-        vector_layer[vector_layer.EGID == egid].to_file(feature_path, driver="GPKG", layer=str(int(egid)))
-    written_files.append(feature_path)  
-    logger.info(f"...done. A file was written: {feature_path}")
+    # # Save the vectors for each EGID in layers in a gpkg !!! Will be deleted at the end !!!
+    # feature_path = os.path.join(output_dir, "roof_segmentation_egid.gpkg")
+    # for egid in vector_layer.EGID.unique():
+    #     vector_layer[vector_layer.EGID == egid].to_file(feature_path, driver="GPKG", layer=str(int(egid)))
+    # written_files.append(feature_path)  
+    # logger.info(f"...done. A file was written: {feature_path}")
 
-    # Save all the vectors in a gpkg 
+    # Save the vectors layer in a gpkg 
+    vector_layer['fid'] = vector_layer.index
     feature_path = os.path.join(output_dir, "roof_segmentation.gpkg")
     vector_layer.to_file(feature_path)
     written_files.append(feature_path)  
@@ -137,9 +135,9 @@ if __name__ == "__main__":
     ROOFS = cfg['roofs']
     OUTPUT_DIR = cfg['output_dir']
     SHP_EXT = cfg['vector_extension']
-    SRS = cfg['srs']
+    CRS = cfg['crs']
 
-    main(WORKING_DIR, EGIDS, ROOFS, OUTPUT_DIR, SHP_EXT, SRS)
+    main(WORKING_DIR, EGIDS, ROOFS, OUTPUT_DIR, SHP_EXT, CRS)
 
     # Stop chronometer  
     toc = time.time()
