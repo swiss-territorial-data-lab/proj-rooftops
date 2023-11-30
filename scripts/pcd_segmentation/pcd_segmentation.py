d928396deebfe4ef78d8fe97a9e7fc86ca13801b
Clemence Herny <clemence.herny@gmail.com>
2023-11-30 13:24:13 +0000

    Correct pcd_segmentation PEP8 style

--- scripts/pcd_segmentation/pcd_segmentation.py
+++ scripts/pcd_segmentation/pcd_segmentation.py
@@ -30,7 +30,7 @@ def main (WORKING_DIR, INPUT_DIR, OUTPUT_DIR,
           EGIDS, 
           distance_threshold, ransac, iterations, eps_planes, min_points_planes, eps_clusters, min_points_clusters,
           number_planes=None, visu=False):
-    """Perform the segmentation of the point cloud in planes and clusters in order to find the roof planes.
+    """Perform segmentation of point cloud in planes and clusters in order to find the roof planes and roof objects.
 
     Args:
         WORKING_DIR (path): working directory
@@ -59,11 +59,11 @@ def main (WORKING_DIR, INPUT_DIR, OUTPUT_DIR,
     written_files = []
 
     # Get the EGIDS of interest
-    egids=pd.read_csv(EGIDS)
+    egids = pd.read_csv(EGIDS)
 
     if not number_planes:
         logger.info('The number of planes for each EGID is deduced from the original layer.')
-    number_planes_ini=number_planes
+    number_planes_ini = number_planes
 
     for egid_info in egids.itertuples():
 
@@ -78,8 +78,8 @@ def main (WORKING_DIR, INPUT_DIR, OUTPUT_DIR,
         # Conversion of numpy array to Open3D format + visualisation
         pcd = o3d.geometry.PointCloud()
         pcd.points = o3d.utility.Vector3dVector(array_pts)
-        # if visu:
-        #     o3d.visualization.draw_geometries([pcd])
+        if visu:
+            o3d.visualization.draw_geometries([pcd])
 
         # Point cloud plane segmentation  
 
@@ -98,24 +98,24 @@ def main (WORKING_DIR, INPUT_DIR, OUTPUT_DIR,
         for i in range(number_planes):
             # Exploration of the best plane candidate + point clustering
             segment_models[i], inliers = remaining_pts.segment_plane(
-                distance_threshold = distance_threshold, ransac_n = ransac, num_iterations = iterations, probability = 1.0,
+                distance_threshold=distance_threshold, ransac_n=ransac, num_iterations=iterations, probability=1.0,
             )
             segments[i] = remaining_pts.select_by_index(inliers)
-            labels = np.array(segments[i].cluster_dbscan(eps = eps_planes, min_points = min_points_planes, print_progress = True))
+            labels = np.array(segments[i].cluster_dbscan(eps=eps_planes, min_points=min_points_planes, print_progress=True))
             candidates = pd.DataFrame({
-                'value': [j for j in np.unique(labels) if j!=-1],
-                'value_count':[len(np.where(labels == j)[0]) for j in np.unique(labels) if j!=-1]
+                'value': [j for j in np.unique(labels) if j!= -1],
+                'value_count':[len(np.where(labels == j)[0]) for j in np.unique(labels) if j!= -1]
             })
-            if len(candidates)==0:
+            if len(candidates) == 0:
                 break
-            elif len(candidates)==1:
-                best_candidate=candidates.iloc[0,0]
+            elif len(candidates) == 1:
+                best_candidate = candidates.iloc[0, 0]
             else:
                 best_candidate = int(candidates.loc[candidates.value_count==candidates.value_count.max(), 'value'].iloc[0])
             logger.info(f"   - The best candidate is: {best_candidate}")
             
             # Select the remaining points in the pcd to find a new plane
-            remaining_pts = remaining_pts.select_by_index(inliers, invert = True) + \
+            remaining_pts = remaining_pts.select_by_index(inliers, invert=True) + \
                 segments[i].select_by_index(list(np.where(labels != best_candidate)[0]))
             segments[i] = segments[i].select_by_index(list(np.where(labels == best_candidate)[0]))  
             
@@ -127,15 +127,15 @@ def main (WORKING_DIR, INPUT_DIR, OUTPUT_DIR,
             # Add segmented planes to a dataframe
             plane = np.asarray(segments[i].points)
             plane_df = pd.DataFrame({'X': plane[:, 0],'Y': plane[:, 1],'Z': plane[:, 2], 'group': i, 'type': 'plane'})
-            planes_df = pd.concat([planes_df, plane_df], ignore_index = True)
+            planes_df = pd.concat([planes_df, plane_df], ignore_index=True)
 
-            if len(remaining_pts.points)<=ransac:
+            if len(remaining_pts.points) <= ransac:
                 break
 
         number_planes = i
 
         # Cluster remaining points (not belonging to a plane) of the pcd after plane segmentation
-        labels = np.array(remaining_pts.cluster_dbscan(eps = eps_clusters, min_points = min_points_clusters))
+        labels = np.array(remaining_pts.cluster_dbscan(eps=eps_clusters, min_points=min_points_clusters))
         if labels.size == 0:
             clusters_df = pd.DataFrame({'X': [], 'Y': [], 'Z': [], 'group': [], 'type': []})
         else: 
@@ -150,7 +150,7 @@ def main (WORKING_DIR, INPUT_DIR, OUTPUT_DIR,
             clusters_df = pd.DataFrame({'X': clusters[:, 0], 'Y': clusters[:, 1], 'Z': clusters[:, 2], 'group': labels, 'type': 'cluster'})
 
         # Merge planes and clusters into a single dataframe 
-        pcd_seg_df = pd.DataFrame(pd.concat([planes_df, clusters_df], ignore_index = True))
+        pcd_seg_df = pd.DataFrame(pd.concat([planes_df, clusters_df], ignore_index=True))
 
         feature_path = os.path.join(OUTPUT_DIR, file_name + '_segmented.csv')
         pcd_seg_df.to_csv(feature_path)
@@ -159,7 +159,7 @@ def main (WORKING_DIR, INPUT_DIR, OUTPUT_DIR,
 
         # Segmented pcd vizualisation
         if visu:
-            pcd.estimate_normals(search_param=o3d.geometry.KDTreeSearchParamHybrid(radius = 0.5, max_nn = 16), fast_normal_computation = True)
+            pcd.estimate_normals(search_param=o3d.geometry.KDTreeSearchParamHybrid(radius=0.5, max_nn=16), fast_normal_computation=True)
             pcd.paint_uniform_color([0.6, 0.6, 0.6])
             o3d.visualization.draw_geometries([segments[i] for i in range(number_planes)] + [remaining_pts])
             print()
@@ -191,8 +191,8 @@ if __name__ == "__main__":
 
     EGIDS = cfg['egids']
 
-    SEGMENTATION=cfg['segmentation']
-    NB_PLANES = SEGMENTATION['planes']['number_planes'] if 'number_planes' in  SEGMENTATION['planes'].keys() else None
+    SEGMENTATION = cfg['segmentation']
+    NB_PLANES = SEGMENTATION['planes']['number_planes'] if 'number_planes' in SEGMENTATION['planes'].keys() else None
     DISTANCE_THRESHOLD = SEGMENTATION['planes']['distance_threshold']
     RANSAC = SEGMENTATION['planes']['ransac']
     ITER = SEGMENTATION['planes']['iterations']
